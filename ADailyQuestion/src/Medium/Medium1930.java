d5e79ab1643be1563cc9bf0e4eb434b0515e4c1a
AJINKYA <ajinkyacodes@gmail.com>
2025-01-04 16:30:26 +0530

    code updated

--- ADailyQuestion/src/Medium/Medium1930.java
+++ ADailyQuestion/src/Medium/Medium1930.java
@@ -2,17 +2,45 @@ package Medium;
 /*
 Link : https://leetcode.com/problems/unique-length-3-palindromic-subsequences/description/
 1930. Unique Length-3 Palindromic Subsequences
+Topics: HashTable, String
 Date: 4 January 2025
+Solution: https://www.youtube.com/watch?v=0rTkpQC8ytY
  */
+import java.util.*;
 public class Medium1930 {
     public static int countPalindromicSubsequence(String s) {
-        String str = "ABC"+s;
-        int output = str.length();
-        return output;
+        int n = s.length();
+
+        // Step 1: Get First & Last occurrences of each char
+        Map<Character, int[]> firstLast = new HashMap<>();
+        for(int i=0; i<n; i++){
+            char c = s.charAt(i);
+            if(!firstLast.containsKey(c)){
+                firstLast.put(c, new int[]{i,i});
+            } else {
+                firstLast.get(c)[1]=i;
+            }
+        }
+
+        // Step 2: Find unique elements count in range
+        int count = 0;
+        for(Map.Entry<Character, int[]> entry : firstLast.entrySet()){
+            int[] range = entry.getValue();
+            if(range[0]==range[1]) continue; // No range to consider
+
+            Set<Character> uniqueChars = new HashSet<>();
+            for(int i = range[0] + 1; i<range[1]; i++){
+                uniqueChars.add(s.charAt(i));
+            }
+            count += uniqueChars.size();
+        }
+        return count;
     }
+    // TC = O(N)
+    // SC = O(1)
 
     public static void main(String[] args) {
-        String a = "XYZ";
+        String a = "abcabdb";
         System.out.println(countPalindromicSubsequence(a));
     }
 }
