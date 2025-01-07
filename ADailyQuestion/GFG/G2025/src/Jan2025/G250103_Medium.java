f80b24267067ffcc798759140326f630cf1ac4ab
AJINKYA <ajinkyacodes@gmail.com>
2025-01-07 14:37:44 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/Jan2025/G250103_Medium.java
@@ -0,0 +1,45 @@
+package Jan2025;
+/*
+Link: https://www.geeksforgeeks.org/problems/count-subarray-with-given-xor/1
+Count Subarrays with given XOR
+Topics: Array, Map
+ */
+import java.util.*;
+public class G250103_Medium {
+    public static long subarrayXor(int arr[], int k) {
+        // Map to store the frequency of prefix XORs
+        Map<Integer, Integer> xorMap = new HashMap<>();
+
+        int count = 0;
+        int xor = 0;
+
+        for (int num : arr) {
+            // Update the running XOR
+            xor ^= num;
+
+            // If the running XOR equals k, increment the count
+            if (xor == k) {
+                count++;
+            }
+
+            // Check if there is a prefix XOR that satisfies the condition
+            int requiredXOR = xor ^ k;
+            if (xorMap.containsKey(requiredXOR)) {
+                count += xorMap.get(requiredXOR);
+            }
+
+            // Update the frequency of the current XOR in the map
+            xorMap.put(xor, xorMap.getOrDefault(xor, 0) + 1);
+        }
+
+        return count;
+    }
+    // TC = O(N)
+    // SC = O(N)
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 2, 6, 4};
+        int k = 6;
+        System.out.println(subarrayXor(arr, k)); // 4
+    }
+}
