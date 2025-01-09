cb880eef13f9dd749bbc0873ab69f668bb32bcf9
AJINKYA <ajinkyacodes@gmail.com>
2025-01-09 16:06:31 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M01_January2025/L250109_Easy2185.java
@@ -0,0 +1,29 @@
+package M01_January2025;
+/*
+Link : https://leetcode.com/problems/counting-words-with-a-given-prefix/description/
+2185. Counting Words With a Given Prefix
+Topics: Array, String, String Matching
+ */
+public class L250109_Easy2185 {
+    public static int prefixCount(String[] words, String pref) {
+        int n = words.length;
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if(words[i].startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    // TC = O(N * M)
+    // SC = O(1)
+
+    public static void main(String[] args) {
+        String[] words = {"pay","attention","practice","attend"};
+        String pref = "at";
+        String[] words2 = {"leetcode","win","loops","success"};
+        String pref2 = "code";
+        System.out.println(prefixCount(words, pref)); // 2
+        System.out.println(prefixCount(words2, pref2)); // 0
+    }
+}
