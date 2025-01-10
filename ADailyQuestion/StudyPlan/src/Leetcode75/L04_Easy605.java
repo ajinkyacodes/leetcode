f0b77d978de5d6a0c80a55090b3839d874e7d5b1
AJINKYA <ajinkyacodes@gmail.com>
2025-01-10 18:52:17 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L04_Easy605.java
@@ -0,0 +1,32 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/can-place-flowers/description/
+605. Can Place Flowers
+Topics, Array, Greedy
+ */
+public class L04_Easy605 {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n==0) return true;
+        int len = flowerbed.length;
+        int count = 0;
+        for(int i=0; i<len; i++) {
+            if(flowerbed[i]==0) {
+                if((i==0 || flowerbed[i-1]==0) && (i==len-1 || flowerbed[i+1]==0)) {
+                    count++;
+                    if(count==n){
+                        return true;
+                    }
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] flowerbed = {1,0,0,0,1};
+        int n = 1;
+        System.out.println(canPlaceFlowers(flowerbed, n)); // true
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1}, 2)); // false
+    }
+}
