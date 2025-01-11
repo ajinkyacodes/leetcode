e1866b8471ac8d0c4020324a04e3baf7f6e3ba75
AJINKYA <ajinkyacodes@gmail.com>
2025-01-11 09:47:53 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M01_January2025/L250111_Medium1400.java
@@ -0,0 +1,38 @@
+package M01_January2025;
+/*
+Link : https://leetcode.com/problems/construct-k-palindrome-strings/description/
+1400. Construct K Palindrome Strings
+Topics: Hash Table, String, Greedy, Counting
+ */
+import java.util.HashMap;
+public class L250111_Medium1400 {
+    public static boolean canConstruct(String s, int k) {
+        int n = s.length();
+        int odd = 0;
+
+        if(n<k) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int i: map.values()) {
+            if(i%2 !=0) odd++;
+        }
+
+        System.out.println(map);
+        System.out.println("odd = "+odd+" and k = "+k);
+
+        return odd<=k;
+    }
+    // TC = O(N)
+    // SC = O(1)
+
+    public static void main(String[] args) {
+        String s = "annabelle";
+        int k = 2;
+        System.out.println(canConstruct(s, k)); // true
+    }
+}
