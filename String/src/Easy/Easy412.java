3f0d4127358eb15eb46ad3907f949252f2bc4e66
AJINKYA <ajinkyacodes@gmail.com>
2025-01-12 16:19:32 +0530

    code added

--- String/src/Easy/Easy412.java
+++ String/src/Easy/Easy412.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class Easy412 {
     public static List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<>();
+        List<String> answer = new ArrayList<>(n);
         for(int i=1; i<=n; i++){
             if(i%3==0 && i%5==0) {
                 answer.add("FizzBuzz");
