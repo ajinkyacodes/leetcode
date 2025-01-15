8dc9db5e39d0ac5988f97bccb5b4eb669229a3f6
AJINKYA <ajinkyacodes@gmail.com>
2025-01-15 15:58:53 +0530

    code added

--- BitManipulation/src/Easy/Easy1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.java
+++ BitManipulation/src/Easy/Easy1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.java
@@ -1,3 +1,5 @@
+package Easy;
+
 /*
 Link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
 1342. Number of Steps to Reduce a Number to Zero
---
+++ BitManipulation/src/Medium/Medium2429_Minimize_XOR.java
@@ -0,0 +1,37 @@
+package Medium;
+/*
+Link: https://leetcode.com/problems/minimize-xor/description/
+2429. Minimize XOR
+Topics: Greedy, Bit Manipulation
+Solution Explained: https://www.youtube.com/watch?v=zSuIRdCuRmw
+ */
+public class Medium2429_Minimize_XOR {
+    public static int minimizeXor(int num1, int num2) {
+        int count = Integer.bitCount(num2);
+        int result = 0;
+
+        // Cancel set bits from highest to lowest weight
+        for(int i=31; i>=0 && count>0; i--) {
+            if((num1 & (1<<i)) != 0) {
+                count--;
+                result += (1<<i);
+            }
+        }
+
+        // Set bits lowest to highest weight
+        for(int i=0; i<32 && count>0; i++) {
+            if((num1 & (1<<i)) ==0) {
+                count--;
+                result += (1<<i);
+            }
+        }
+        return result;
+    }
+    // TC = O(log n) => O(1)  (Since int -> 32 bits)
+    // SC = O(1)
+
+    public static void main(String[] args) {
+        System.out.println(minimizeXor(3,5)); // 5
+        System.out.println(minimizeXor(1,12)); // 5
+    }
+}
