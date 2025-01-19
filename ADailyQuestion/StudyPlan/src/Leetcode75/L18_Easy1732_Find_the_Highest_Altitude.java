ed6ca9caa7f14215dde88a13111303550f14e484
AJINKYA <ajinkyacodes@gmail.com>
2025-01-19 15:14:08 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L18_Easy1732_Find_the_Highest_Altitude.java
@@ -0,0 +1,36 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/find-the-highest-altitude/description/
+1732. Find the Highest Altitude
+Topic: Prefix Sum
+ */
+public class L18_Easy1732_Find_the_Highest_Altitude {
+    public static int largestAltitude(int[] gain) {
+        int n = gain.length;
+        int maxHeight = 0;
+        int[] alt = new int[n+1];
+        alt[0] = 0;
+
+        // Storing altitudes
+        for(int i=1; i<n+1; i++){
+            alt[i] = alt[i-1] + gain[i-1];
+        }
+
+        // Finding the highest altitude
+        for(int i=0; i<alt.length; i++){
+            maxHeight = Math.max(maxHeight, alt[i]);
+        }
+
+        return maxHeight;
+    }
+    // TC = O(n)
+    // SC = O(n)
+
+    public static void main(String[] args) {
+        int[] gain1 = {-5,1,5,0,-7};
+        System.out.println(largestAltitude(gain1)); // 1
+
+        int[] gain2 = {-4,-3,-2,-1,4,3,2};
+        System.out.println(largestAltitude(gain2)); // 0
+    }
+}
---
+++ PrefixSum/src/Easy/Easy724_Find_Pivot_Index.java
@@ -0,0 +1,42 @@
+package Easy;
+/*
+Link: https://leetcode.com/problems/find-pivot-index/description/
+724. Find Pivot Index
+Topic: Prefix Sum
+ */
+public class Easy724_Find_Pivot_Index {
+    public static int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int totalSum = 0;
+
+        // Calculating Total Sum
+        for(int i=0; i<n; i++) {
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+        int rightSum = totalSum;
+
+        // Traversing to compare left and right sum
+        for(int i=0; i<n; i++){
+            rightSum -= nums[i];
+            if(leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+    // TC = O(n)
+    // SC = O(1)
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,7,3,6,5,6};
+        int[] nums2 = {1,2,3};
+        int[] nums3 = {2,1,-1};
+        System.out.println(pivotIndex(nums1)); // 3
+        System.out.println(pivotIndex(nums2)); // -1
+        System.out.println(pivotIndex(nums3)); // 0
+    }
+}
