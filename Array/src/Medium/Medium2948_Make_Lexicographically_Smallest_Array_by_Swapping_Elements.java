d44445acb0c571580cc991e64f74e002d4ddc2f2
AJINKYA <ajinkyacodes@gmail.com>
2025-01-25 23:05:14 +0530

    code added

---
+++ Array/src/Medium/Medium2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements.java
@@ -0,0 +1,66 @@
+package Medium;
+/*
+Link: https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/description/
+2948. Make Lexicographically Smallest Array by Swapping Elements
+Topic: Array, Hash Table
+ */
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Medium2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements {
+    public static int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int[] numsSorted = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) numsSorted[i] = nums[i];
+        Arrays.sort(numsSorted);
+
+        int currGroup = 0;
+        HashMap<Integer, Integer> numToGroup = new HashMap<>();
+        numToGroup.put(numsSorted[0], currGroup);
+
+        HashMap<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+        groupToList.put(
+                currGroup,
+                new LinkedList<Integer>(Arrays.asList(numsSorted[0]))
+        );
+
+        for (int i = 1; i < nums.length; i++) {
+            if (Math.abs(numsSorted[i] - numsSorted[i - 1]) > limit) {
+                // new group
+                currGroup++;
+            }
+
+            // assign current element to group
+            numToGroup.put(numsSorted[i], currGroup);
+
+            // add element to sorted group list
+            if (!groupToList.containsKey(currGroup)) {
+                groupToList.put(currGroup, new LinkedList<Integer>());
+            }
+            groupToList.get(currGroup).add(numsSorted[i]);
+        }
+
+        // Overwrite each element with the next element in its corresponding group
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int group = numToGroup.get(num);
+            nums[i] = groupToList.get(group).pop();
+        }
+
+        return nums;
+    }
+    // Time = O(n log n)
+    // Space = O(n)
+
+    public static void main(String[] args) {
+        int[] result1 = lexicographicallySmallestArray(new int[]{1,5,3,9,8}, 2);
+        System.out.println(Arrays.toString(result1)); // [1,3,5,8,9]
+
+        int[] result2 = lexicographicallySmallestArray(new int[]{1,7,6,18,2,1}, 3);
+        System.out.println(Arrays.toString(result2)); // [1,6,7,18,1,2]
+
+        int[] result3 = lexicographicallySmallestArray(new int[]{1,7,28,19,10}, 3);
+        System.out.println(Arrays.toString(result3)); // [1,7,28,19,10]
+    }
+}
