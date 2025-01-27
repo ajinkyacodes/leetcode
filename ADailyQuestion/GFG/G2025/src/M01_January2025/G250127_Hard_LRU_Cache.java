72bc67523a87d1e5f7a0de2a6d5e170e7b47df73
AJINKYA <ajinkyacodes@gmail.com>
2025-01-27 23:37:49 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/M01_January2025/G250127_Hard_LRU_Cache.java
@@ -0,0 +1,69 @@
+package M01_January2025;
+/*
+Link: https://www.geeksforgeeks.org/problems/lru-cache/1
+LRU Cache
+Topic: Doubly Linked List
+ */
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class LRUDesign {
+
+    private static List<String> inputLine(Scanner sc) {
+        return Arrays.asList(sc.nextLine().split(" "));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+
+        while (t-- > 0) {
+            int capacity = Integer.parseInt(sc.nextLine());
+            LRUCache cache = new LRUCache(capacity);
+
+            int queries = Integer.parseInt(sc.nextLine());
+            while (queries-- > 0) {
+                List<String> vec = inputLine(sc);
+                if (vec.get(0).equals("PUT")) {
+                    int key = Integer.parseInt(vec.get(1));
+                    int value = Integer.parseInt(vec.get(2));
+                    cache.put(key, value);
+                } else {
+                    int key = Integer.parseInt(vec.get(1));
+                    System.out.print(cache.get(key) + " ");
+                }
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+class LRUCache {
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> cache;
+
+    LRUCache(int cap) {
+        this.capacity = cap;
+        this.cache = new LinkedHashMap<>(cap, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
+// Time = O(1)
+// Space = O(1)
+
+public class G250127_Hard_LRU_Cache {
+}
+
+
