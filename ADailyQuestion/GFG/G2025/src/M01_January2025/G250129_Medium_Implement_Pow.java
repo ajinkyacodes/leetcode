09b9cc2588ed0af2a4ccec6d8c07270116a9501e
AJINKYA <ajinkyacodes@gmail.com>
2025-01-29 23:45:40 +0530

    code added

--- ADailyQuestion/GFG/G2025/src/M01_January2025/G250129_Medium_Implement_Pow.java
+++ ADailyQuestion/GFG/G2025/src/M01_January2025/G250129_Medium_Implement_Pow.java
@@ -1,4 +1,28 @@
 package M01_January2025;
-
+/*
+Link: https://www.geeksforgeeks.org/problems/powx-n/1
+Implement Pow
+ */
 public class G250129_Medium_Implement_Pow {
+    public static double power(double b, int e) {
+        if (e == 0) return 1.0;
+        double half = power(b, e / 2);
+        return e % 2 == 0 ? half * half : (e > 0 ? b * half * half : (1.0 / b) * half * half);
+    }
+    // time = O(log e)
+    // space = O(log e)
+
+    public static void main(String[] args) {
+        double b1 = 3.00000;
+        int e1 = 5;
+        System.out.println(power(b1,e1)); // 243.00000
+
+        double b2 = 0.16638;
+        int e2 = 3;
+        System.out.println(power(b2,e2)); // 0.16638
+
+        double b3 = -0.67000;
+        int e3 = -7;
+        System.out.println(power(b3,e3)); // -16.49971
+    }
 }
--- ADailyQuestion/StudyPlan/src/Leetcode75/L29_Medium2095_Delete_the_Middle_Node_of_a_Linked_List.java
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L29_Medium2095_Delete_the_Middle_Node_of_a_Linked_List.java
@@ -1,4 +1,48 @@
 package Leetcode75;
+/*
+Link: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+2095. Delete the Middle Node of a Linked List
+Topic: Linked List
+ */
 
+
+ // Definition for singly-linked list.
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
 public class L29_Medium2095_Delete_the_Middle_Node_of_a_Linked_List {
+    public ListNode deleteMiddle(ListNode head) {
+        //Approach: 2 Passes
+
+        // Edge case: return None if there is only one node
+        if (head.next == null)
+            return null;
+
+        int count = 0;
+        ListNode p1 = head, p2 = head;
+
+        // First pass, count the number of nodes in the linked list using 'p1'.
+        while (p1 != null) {
+            count += 1;
+            p1 = p1.next;
+        }
+
+        // Get the index of the node to be deleted.
+        int middleIndex = count / 2;
+
+        // Second pass, let 'p2' move toward the predecessor of the middle node.
+        for (int i = 0; i < middleIndex - 1; ++i)
+            p2 = p2.next;
+
+        // Delete the middle node and return 'head'.
+        p2.next = p2.next.next;
+        return head;
+    }
+    // time = O(n)
+    // space = O(1)
 }
