b028592543049aa59cff1b4a4dd8081b524a5238
AJINKYA <ajinkyacodes@gmail.com>
2025-01-29 23:45:37 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/M01_January2025/G250129_Medium_Implement_Pow.java
@@ -0,0 +1,4 @@
+package M01_January2025;
+
+public class G250129_Medium_Implement_Pow {
+}
---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L29_Medium2095_Delete_the_Middle_Node_of_a_Linked_List.java
@@ -0,0 +1,4 @@
+package Leetcode75;
+
+public class L29_Medium2095_Delete_the_Middle_Node_of_a_Linked_List {
+}
