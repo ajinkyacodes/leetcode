5f5d870ccf3dd7cb53d12d03bbab9a5351139187
AJINKYA <ajinkyacodes@gmail.com>
2025-01-30 23:57:47 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L30_Medium328_Odd_Even_Linked_List.java
@@ -0,0 +1,33 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/odd-even-linked-list/description/
+328. Odd Even Linked List
+Topic: Linked List
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class L30_Medium328_Odd_Even_Linked_List {
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+    // time = O(n)
+    // space = O(1)
+}
