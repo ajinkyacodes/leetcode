27e12d4ea14f359a57e26fda209752e909a5e4c2
AJINKYA <ajinkyacodes@gmail.com>
2025-01-31 09:14:56 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L31_Easy206_Reverse_Linked_List.java
@@ -0,0 +1,33 @@
+package Leetcode75;
+/*
+Link:
+206. Reverse Linked List
+Topic: Linked List
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class L31_Easy206_Reverse_Linked_List {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+    // time = O(n)
+    // space = O(1)
+}
