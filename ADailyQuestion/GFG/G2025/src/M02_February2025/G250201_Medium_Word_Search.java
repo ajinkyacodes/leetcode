c1ca288944fd13fa958d3be5eee74d20a43f5631
AJINKYA <ajinkyacodes@gmail.com>
2025-02-01 23:40:12 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/M02_February2025/G250201_Medium_Word_Search.java
@@ -0,0 +1,34 @@
+package M02_February2025;
+/*
+Link:
+Word Search
+Topic: Recursion, DFS, Graph
+ */
+public class G250201_Medium_Word_Search {
+    public static boolean isWordExist(char[][] b, String w) {
+        for (int i = 0; i < b.length; i++)
+            for (int j = 0; j < b[0].length; j++)
+                if (b[i][j] == w.charAt(0) && dfs(b, w, i, j, 0))
+                    return true;
+        return false;
+    }
+    // time =  O(m * n * 4^k)
+    // space = O(k)
+
+    private static boolean dfs(char[][] b, String w, int i, int j, int k) {
+        if (k == w.length()) return true;
+        if (i < 0 || j < 0 || i >= b.length || j >= b[0].length || b[i][j] != w.charAt(k)) return false;
+        char t = b[i][j];
+        b[i][j] = '#';
+        boolean f = dfs(b, w, i - 1, j, k + 1) || dfs(b, w, i + 1, j, k + 1) ||
+                dfs(b, w, i, j - 1, k + 1) || dfs(b, w, i, j + 1, k + 1);
+        b[i][j] = t;
+        return f;
+    }
+
+    public static void main(String[] args) {
+        char[][] mat = {{'T', 'E', 'E'}, {'S', 'G', 'K'}, {'T', 'E', 'L'}};
+        String word = "GEEK";
+        System.out.println(isWordExist(mat, word)); // true
+    }
+}
