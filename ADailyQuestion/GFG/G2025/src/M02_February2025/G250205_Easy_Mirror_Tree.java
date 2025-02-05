074403e30650586c29e8791f0d429c6440f47f4a
AJINKYA <ajinkyacodes@gmail.com>
2025-02-05 21:39:28 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/M02_February2025/G250205_Easy_Mirror_Tree.java
@@ -0,0 +1,30 @@
+package M02_February2025;
+/*
+Link:
+Mirror Tree
+Topic: Tree
+ */
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+public class G250205_Easy_Mirror_Tree {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        if (node == null) return;
+        mirror(node.left);
+        mirror(node.right);
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
