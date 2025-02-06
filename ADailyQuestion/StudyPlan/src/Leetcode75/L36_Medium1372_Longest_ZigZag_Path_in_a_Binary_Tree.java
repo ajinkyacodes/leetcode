4bcb6305463044610d6cde4202831f53fd286c0b
AJINKYA <ajinkyacodes@gmail.com>
2025-02-07 02:43:43 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L36_Medium1372_Longest_ZigZag_Path_in_a_Binary_Tree.java
@@ -0,0 +1,46 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/
+1372. Longest ZigZag Path in a Binary Tree
+Topic: Depth First Search
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class L36_Medium1372_Longest_ZigZag_Path_in_a_Binary_Tree {
+    int pathLength = 0;
+    private void dfs(TreeNode node, boolean goLeft, int steps) {
+        if (node == null) {
+            return;
+        }
+        pathLength = Math.max(pathLength, steps);
+        if (goLeft) {
+            dfs(node.left, false, steps + 1);
+            dfs(node.right, true, 1);
+        } else {
+            dfs(node.left, false, 1);
+            dfs(node.right, true, steps + 1);
+        }
+    }
+
+    public int longestZigZag(TreeNode root) {
+        dfs(root, true, 0);
+        return pathLength;
+    }
+    // time = O(n)
+    // space = O(n)
+}
