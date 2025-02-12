c95ecf9298720bbb349ded85d043a8c23ac0ad17
AJINKYA <ajinkyacodes@gmail.com>
2025-02-12 22:23:07 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/M02_February2025/G250212_Medium_k_th_Smallest_in_BST.java
@@ -0,0 +1,27 @@
+package M02_February2025;
+/*
+Link: https://www.geeksforgeeks.org/problems/find-k-th-smallest-element-in-bst/1
+k-th Smallest in BST
+ */
+public class G250212_Medium_k_th_Smallest_in_BST {
+    public int kthSmallest(Node root, int k) {
+        while (root != null) {
+            if (root.left == null) {
+                if (--k == 0) return root.data;
+                root = root.right;
+            } else {
+                Node pre = root.left;
+                while (pre.right != null && pre.right != root) pre = pre.right;
+                if (pre.right == null) {
+                    pre.right = root;
+                    root = root.left;
+                } else {
+                    pre.right = null;
+                    if (--k == 0) return root.data;
+                    root = root.right;
+                }
+            }
+        }
+        return -1;
+    }
+}
