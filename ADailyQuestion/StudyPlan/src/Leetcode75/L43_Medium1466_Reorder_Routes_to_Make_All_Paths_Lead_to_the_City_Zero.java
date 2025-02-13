d7ca25dc2f3cbb5349566fedabeb3e203bb9724b
AJINKYA <ajinkyacodes@gmail.com>
2025-02-13 22:11:34 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L43_Medium1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.java
@@ -0,0 +1,38 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
+1466. Reorder Routes to Make All Paths Lead to the City Zero
+Topic: DFS
+ */
+import java.util.*;
+public class L43_Medium1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero {
+    int count = 0;
+
+    public void dfs(int node, int parent, Map<Integer, List<List<Integer>>> adj) {
+        if (!adj.containsKey(node)) {
+            return;
+        }
+        for (List<Integer> nei : adj.get(node)) {
+            int neighbor = nei.get(0);
+            int sign = nei.get(1);
+            if (neighbor != parent) {
+                count += sign;
+                dfs(neighbor, node, adj);
+            }
+        }
+    }
+
+    public int minReorder(int n, int[][] connections) {
+        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
+        for (int[] connection : connections) {
+            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[1], 1));
+            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[0], 0));
+        }
+        dfs(0, -1, adj);
+        return count;
+    }
+    // time = O(n)
+    // space = O(n)
+}
