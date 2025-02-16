62495296d2e92c892bdf8803245b7a2efcf58444
AJINKYA <ajinkyacodes@gmail.com>
2025-02-17 03:23:03 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/SQL50/S46_Easy196_Delete_Duplicate_Emails.sql
@@ -0,0 +1,8 @@
+# Link: https://leetcode.com/problems/delete-duplicate-emails/description/
+# 196. Delete Duplicate Emails
+
+# Write your MySQL query statement below
+DELETE p1 FROM Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id;
