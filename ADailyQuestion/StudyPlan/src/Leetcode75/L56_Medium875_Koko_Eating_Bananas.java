97ca19c9d8f7988e570acc61eeb024939abb32b6
AJINKYA <ajinkyacodes@gmail.com>
2025-02-24 18:01:47 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L56_Medium875_Koko_Eating_Bananas.java
@@ -0,0 +1,41 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/koko-eating-bananas/description/
+875. Koko Eating Bananas
+Approach: Binary Search
+ */
+public class L56_Medium875_Koko_Eating_Bananas {
+    public int minEatingSpeed(int[] piles, int h) {
+        // Initalize the left and right boundaries
+        int left = 1, right = 1;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+            // Get the middle index between left and right boundary indexes.
+            // hourSpent stands for the total hour Koko spends.
+            int middle = (left + right) / 2;
+            int hourSpent = 0;
+
+            // Iterate over the piles and calculate hourSpent.
+            // We increase the hourSpent by ceil(pile / middle)
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / middle);
+            }
+
+            // Check if middle is a workable speed, and cut the search space by half.
+            if (hourSpent <= h) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        // Once the left and right boundaries coincide, we find the target value,
+        // that is, the minimum workable eating speed.
+        return right;
+    }
+    // time = O(n⋅logm)
+    // space = O(1)
+}
