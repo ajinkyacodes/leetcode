6584496432a1f05ecefbc169c2d91a27f22c79a6
AJINKYA <ajinkyacodes@gmail.com>
2025-03-02 08:38:17 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M03_March2025/L250302_Easy2570_Merge_Two_2D_Arrays_by_Summing_Values.java
@@ -0,0 +1,59 @@
+package M03_March2025;
+/*
+Link: https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/description/
+2570. Merge Two 2D Arrays by Summing Values
+ */
+import java.util.*;
+public class L250302_Easy2570_Merge_Two_2D_Arrays_by_Summing_Values {
+    // Approach: Two Pointers
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int N1 = nums1.length, N2 = nums2.length;
+        int ptr1 = 0, ptr2 = 0;
+
+        List<int[]> mergedArray = new ArrayList<>();
+        while (ptr1 < N1 && ptr2 < N2) {
+            // If the id is same, add the values and insert to the result.
+            // Increment both pointers.
+            if (nums1[ptr1][0] == nums2[ptr2][0]) {
+                mergedArray.add(
+                        new int[] {
+                                nums1[ptr1][0],
+                                nums1[ptr1][1] + nums2[ptr2][1],
+                        }
+                );
+                ptr1++;
+                ptr2++;
+            } else if (nums1[ptr1][0] < nums2[ptr2][0]) {
+                // If the id in nums1 is smaller, add it to result and increment the pointer
+                mergedArray.add(nums1[ptr1]);
+                ptr1++;
+            } else {
+                // If the id in nums2 is smaller, add it to result and increment the pointer
+                mergedArray.add(nums2[ptr2]);
+                ptr2++;
+            }
+        }
+
+        // If pairs are remaining in the nums1, then add them to the result.
+        while (ptr1 < N1) {
+            mergedArray.add(nums1[ptr1]);
+            ptr1++;
+        }
+
+        // If pairs are remaining in the nums2, then add them to the result.
+        while (ptr2 < N2) {
+            mergedArray.add(nums2[ptr2]);
+            ptr2++;
+        }
+
+        // Convert List<int[]> to int[][]
+        int[][] result = new int[mergedArray.size()][2];
+        for (int i = 0; i < mergedArray.size(); i++) {
+            result[i] = mergedArray.get(i);
+        }
+
+        return result;
+    }
+    // time = O(N1+N2)
+    // space = O(N1+N2)
+}
