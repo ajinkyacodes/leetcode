636fb5e969a7274db2ad12df4993bd877333da33
AJINKYA <ajinkyacodes@gmail.com>
2025-03-07 22:58:01 +0530

    code added

---
+++ ADailyQuestion/StudyPlan/src/Leetcode75/L67_Easy338_Counting_Bits.java
@@ -0,0 +1,17 @@
+package Leetcode75;
+/*
+Link: https://leetcode.com/problems/counting-bits/description/
+338. Counting Bits
+ */
+public class L67_Easy338_Counting_Bits {
+    // Approach: DP + Last Set Bit
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int x = 1; x <= num; ++x) {
+            ans[x] = ans[x & (x - 1)] + 1;
+        }
+        return ans;
+    }
+    // time = O(n)
+    // space = O(1)
+}
