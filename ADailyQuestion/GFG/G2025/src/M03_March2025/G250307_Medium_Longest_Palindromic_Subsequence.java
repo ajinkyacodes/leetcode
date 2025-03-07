122a3d6923b2a0707a16320b5793f54eabd28a29
AJINKYA <ajinkyacodes@gmail.com>
2025-03-07 22:47:52 +0530

    code added

---
+++ ADailyQuestion/GFG/G2025/src/M03_March2025/G250307_Medium_Longest_Palindromic_Subsequence.java
@@ -0,0 +1,25 @@
+package M03_March2025;
+
+/*
+Link: https://www.geeksforgeeks.org/problems/longest-palindromic-subsequence-1612327878/1
+Longest Palindromic Subsequence
+ */
+
+public class G250307_Medium_Longest_Palindromic_Subsequence {
+    public int longestPalinSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j))
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+    // tme = O(n^2)
+    // space = O(n^2)
+}
