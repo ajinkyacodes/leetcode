865638083d8330d599e1640936063b009a91c5df
AJINKYA <ajinkyacodes@gmail.com>
2025-03-13 14:35:20 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M03_March2025/L250312_Medium_452_Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -0,0 +1,34 @@
+package M03_March2025;
+import java.util.*;
+public class L250312_Medium_452_Minimum_Number_of_Arrows_to_Burst_Balloons {
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) return 0;
+
+        // sort by x_end
+        Arrays.sort(points, (o1, o2) -> {
+            // We can't simply use the o1[1] - o2[1] trick, as this will cause an
+            // integer overflow for very large or small values.
+            if (o1[1] == o2[1]) return 0;
+            if (o1[1] < o2[1]) return -1;
+            return 1;
+        });
+
+        int arrows = 1;
+        int xStart, xEnd, firstEnd = points[0][1];
+        for (int[] p: points) {
+            xStart = p[0];
+            xEnd = p[1];
+
+            // If the current balloon starts after the end of another one,
+            // one needs one more arrow
+            if (firstEnd < xStart) {
+                arrows++;
+                firstEnd = xEnd;
+            }
+        }
+
+        return arrows;
+    }
+    // time = O(NlogN)
+    // space = O(N)
+}
