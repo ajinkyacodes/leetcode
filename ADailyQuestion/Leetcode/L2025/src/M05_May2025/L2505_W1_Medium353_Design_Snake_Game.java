819af6b2c44300318c8f8050ab8f8110ec8952e0
AJINKYA <ajinkyacodes@gmail.com>
2025-05-04 14:35:04 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M05_May2025/L2505_W1_Medium353_Design_Snake_Game.java
@@ -0,0 +1,10 @@
+package M05_May2025;
+/*
+Link: https://leetcode.com/problems/design-snake-game/description/
+353. Design Snake Game
+ */
+public class L2505_W1_Medium353_Design_Snake_Game {
+    // solved on leetcode
+}
+
+
