841af48046efaf54d2b36df7f28fc1b5dc30111f
AJINKYA <ajinkyacodes@gmail.com>
2025-05-04 12:00:42 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M04_April2025/L250430_Easy1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,28 @@
+package M04_April2025;
+/*
+Link: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+1295. Find Numbers with Even Number of Digits
+ */
+public class L250430_Easy1295_Find_Numbers_with_Even_Number_of_Digits {
+    public static int countDigits(int n) {
+        if(n==0) return 1;
+        int count = 0;
+        while(n!=0) {
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(countDigits(nums[i])%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+    // TC = O(n*d) Where d => maximum number of digits
+    // SC = O(1)
+}
