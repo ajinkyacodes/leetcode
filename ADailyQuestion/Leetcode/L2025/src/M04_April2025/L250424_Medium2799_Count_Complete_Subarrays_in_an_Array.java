fa6346dbc3044694524b8b14a88e98f543175308
AJINKYA <ajinkyacodes@gmail.com>
2025-05-04 11:42:57 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M04_April2025/L250424_Medium2799_Count_Complete_Subarrays_in_an_Array.java
@@ -0,0 +1,38 @@
+package M04_April2025;
+/*
+Link: https://leetcode.com/problems/count-complete-subarrays-in-an-array/description/
+2799. Count Complete Subarrays in an Array
+ */
+import java.util.*;
+public class L250424_Medium2799_Count_Complete_Subarrays_in_an_Array {
+    public int countCompleteSubarrays(int[] nums) {
+        int res = 0;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        int n = nums.length;
+        int right = 0;
+        int distinct = new HashSet<>(
+                Arrays.asList(Arrays.stream(nums).boxed().toArray(Integer[]::new))
+        ).size();
+
+        for (int left = 0; left < n; left++) {
+            if (left > 0) {
+                int remove = nums[left - 1];
+                cnt.put(remove, cnt.get(remove) - 1);
+                if (cnt.get(remove) == 0) {
+                    cnt.remove(remove);
+                }
+            }
+            while (right < n && cnt.size() < distinct) {
+                int add = nums[right];
+                cnt.put(add, cnt.getOrDefault(add, 0) + 1);
+                right++;
+            }
+            if (cnt.size() == distinct) {
+                res += (n - right + 1);
+            }
+        }
+        return res;
+    }
+    // time = O(n)
+    // space = O(n)
+}
