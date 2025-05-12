607d58a517d5541e153ece01965e3b374242b3c0
AJINKYA <ajinkyacodes@gmail.com>
2025-05-12 23:57:00 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M05_May2025/L250512_Easy2094_Finding_3_Digit_Even_Numbers.java
@@ -0,0 +1,37 @@
+package M05_May2025;
+/*
+Link: https://leetcode.com/problems/finding-3-digit-even-numbers/description/
+2094. Finding 3-Digit Even Numbers
+ */
+import java.util.*;
+public class L250512_Easy2094_Finding_3_Digit_Even_Numbers {
+    public int[] findEvenNumbers(int[] digits) {
+        Set<Integer> nums = new HashSet<>();
+        int n = digits.length;
+        // Traverse the indices of three digits
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    // Determine whether it meets the condition of the target even number
+                    if (i == j || j == k || i == k) {
+                        continue;
+                    }
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if (num >= 100 && num % 2 == 0) {
+                        nums.add(num);
+                    }
+                }
+            }
+        }
+        // Converted to an array sorted in ascending order
+        List<Integer> res = new ArrayList<>(nums);
+        Collections.sort(res);
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); ++i) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+    // time = O(n^3 + MlogM)
+    // space = NA
+}
