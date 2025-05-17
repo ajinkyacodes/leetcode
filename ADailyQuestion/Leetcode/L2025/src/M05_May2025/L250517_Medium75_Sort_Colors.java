3741fbebdf76f470c5572f132e43fb677c7d02f8
AJINKYA <ajinkyacodes@gmail.com>
2025-05-17 23:56:22 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M05_May2025/L250517_Medium75_Sort_Colors.java
@@ -0,0 +1,34 @@
+package M05_May2025;
+/*
+Link: https://leetcode.com/problems/sort-colors/description/
+75. Sort Colors
+ */
+public class L250517_Medium75_Sort_Colors {
+    public void sortColors(int[] nums) {
+        // For all idx < i : nums[idx < i] = 0
+        // j is an index of elements under consideration
+        int p0 = 0, curr = 0;
+
+        // For all idx > k : nums[idx > k] = 2
+        int p2 = nums.length - 1;
+
+        int tmp;
+        while (curr <= p2) {
+            if (nums[curr] == 0) {
+                // Swap p0-th and curr-th elements
+                // i++ and j++
+                tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            } else if (nums[curr] == 2) {
+                // Swap k-th and curr-th elements
+                // p2--
+                tmp = nums[curr];
+                nums[curr] = nums[p2];
+                nums[p2--] = tmp;
+            } else curr++;
+        }
+    }
+    // time = O(n)
+    // space = O(1)
+}
