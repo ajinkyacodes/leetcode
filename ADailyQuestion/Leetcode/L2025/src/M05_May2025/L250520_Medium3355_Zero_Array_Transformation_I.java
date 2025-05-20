71df46a094709a233844620d39417d4854d895d7
AJINKYA <ajinkyacodes@gmail.com>
2025-05-21 00:18:50 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M05_May2025/L250520_Medium3355_Zero_Array_Transformation_I.java
@@ -0,0 +1,30 @@
+package M05_May2025;
+/*
+Link: https://leetcode.com/problems/zero-array-transformation-i/description/
+3355. Zero Array Transformation I
+ */
+public class L250520_Medium3355_Zero_Array_Transformation_I {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int[] deltaArray = new int[nums.length + 1];
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
+        }
+        int[] operationCounts = new int[deltaArray.length];
+        int currentOperations = 0;
+        for (int i = 0; i < deltaArray.length; i++) {
+            currentOperations += deltaArray[i];
+            operationCounts[i] = currentOperations;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // time = O(n+m)
+    // space = O(n)
+}
