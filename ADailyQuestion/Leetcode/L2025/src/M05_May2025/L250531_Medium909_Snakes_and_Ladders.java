b52347d507d1766f8b4457d6d361fb875ce91968
AJINKYA <ajinkyacodes@gmail.com>
2025-05-31 20:14:44 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M05_May2025/L250531_Medium909_Snakes_and_Ladders.java
@@ -0,0 +1,44 @@
+package M05_May2025;
+/*
+Link: https://leetcode.com/problems/snakes-and-ladders/description/
+909. Snakes and Ladders
+ */
+import java.util.*;
+public class L250531_Medium909_Snakes_and_Ladders {
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        Pair<Integer, Integer>[] cells = new Pair[n * n + 1];
+        int label = 1;
+        Integer[] columns = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            columns[i] = i;
+        }
+        for (int row = n - 1; row >= 0; row--) {
+            for (int column : columns) {
+                cells[label++] = new Pair<>(row, column);
+            }
+            Collections.reverse(Arrays.asList(columns));
+        }
+        int[] dist = new int[n * n + 1];
+        Arrays.fill(dist, -1);
+        Queue<Integer> q = new LinkedList<Integer>();
+        dist[1] = 0;
+        q.add(1);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
+                /*
+                int row = cells[next].getKey(), column = cells[next].getValue();
+                int destination = board[row][column] != -1 ? board[row][column] : next;
+                if (dist[destination] == -1) {
+                    dist[destination] = dist[curr] + 1;
+                    q.add(destination);
+                }
+                */
+            }
+        }
+        return dist[n * n];
+    }
+    // time = O(n^2).
+    // space = O(n^2).
+}
