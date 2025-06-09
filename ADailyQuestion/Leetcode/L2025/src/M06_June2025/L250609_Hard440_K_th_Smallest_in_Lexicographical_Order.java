1bf13d0d5b165f78193050b08e074c8361510ad5
AJINKYA <ajinkyacodes@gmail.com>
2025-06-09 16:50:56 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M06_June2025/L250609_Hard440_K_th_Smallest_in_Lexicographical_Order.java
@@ -0,0 +1,40 @@
+package M06_June2025;
+/*
+Link: https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description/
+440. K-th Smallest in Lexicographical Order
+ */
+public class L250609_Hard440_K_th_Smallest_in_Lexicographical_Order {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+
+        while (k > 0) {
+            int step = countSteps(n, curr, curr + 1);
+            // If the steps are less than or equal to k, we skip this prefix's subtree
+            if (step <= k) {
+                // Move to the next prefix and decrease k by the number of steps we skip
+                curr++;
+                k -= step;
+            } else {
+                // Move to the next level of the tree and decrement k by 1
+                curr *= 10;
+                k--;
+            }
+        }
+
+        return curr;
+    }
+
+    // To count how many numbers exist between prefix1 and prefix2
+    private int countSteps(int n, long prefix1, long prefix2) {
+        int steps = 0;
+        while (prefix1 <= n) {
+            steps += Math.min(n + 1, prefix2) - prefix1;
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+        return steps;
+    }
+    // time = O(log(n)^2)
+    // space = O(1)
+}
