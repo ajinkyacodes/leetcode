6e6692556a83a07a7e13c76edb444b5be66f5a7e
AJINKYA <ajinkyacodes@gmail.com>
2025-06-28 17:05:50 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M06_June2025/L250628_Easy2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
@@ -0,0 +1,27 @@
+package M06_June2025;
+/*
+Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/description/
+2099. Find Subsequence of Length K With the Largest Sum
+ */
+import java.util.*;
+public class L250628_Easy2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum {
+    public int[] maxSubsequence(int[] nums, int k) {
+        int n = nums.length;
+        int[][] vals = new int[n][2]; // two-dimensional array stores index and value
+        for (int i = 0; i < n; ++i) {
+            vals[i][0] = i; // store index
+            vals[i][1] = nums[i]; // store value
+        }
+        // sort by numerical value in descending order
+        Arrays.sort(vals, (x1, x2) -> Integer.compare(x2[1], x1[1]));
+        // select the first k elements and sort them in ascending order by index
+        Arrays.sort(vals, 0, k, (x1, x2) -> Integer.compare(x1[0], x2[0]));
+        int[] res = new int[k]; // target subsequence
+        for (int i = 0; i < k; ++i) {
+            res[i] = vals[i][1];
+        }
+        return res;
+    }
+    // time = O(nlogn).
+    // space = O(n).
+}
