7dfad4de879eb4a6ed653e75b72a0958a0bf85dc
AJINKYA <ajinkyacodes@gmail.com>
2025-07-09 10:58:50 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M07_July2025/L250709_Medium3439_Reschedule_Meetings_for_Maximum_Free_Time_I.java
@@ -0,0 +1,29 @@
+package M07_July2025;
+/*
+Link: https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-i/description/
+3439. Reschedule Meetings for Maximum Free Time I
+ */
+public class L250709_Medium3439_Reschedule_Meetings_for_Maximum_Free_Time_I {
+    public int maxFreeTime(
+            int eventTime,
+            int k,
+            int[] startTime,
+            int[] endTime
+    ) {
+        int n = startTime.length;
+        int res = 0;
+        int t = 0;
+        for (int i = 0; i < n; i++) {
+            t += endTime[i] - startTime[i];
+            int left = i <= k - 1 ? 0 : endTime[i - k];
+            int right = i == n - 1 ? eventTime : startTime[i + 1];
+            res = Math.max(res, right - left - t);
+            if (i >= k - 1) {
+                t -= endTime[i - k + 1] - startTime[i - k + 1];
+            }
+        }
+        return res;
+    }
+    // time = O(n)
+    // space = O(1)
+}
