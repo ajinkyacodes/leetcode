c6599042aae5fe6e11027dbbfb1217633c0f1dc6
AJINKYA <ajinkyacodes@gmail.com>
2025-07-14 22:06:56 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M07_July2025/L250714_Easy1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,19 @@
+package M07_July2025;
+/*
+Link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+1290. Convert Binary Number in a Linked List to Integer
+ */
+public class L250714_Easy1290_Convert_Binary_Number_in_a_Linked_List_to_Integer {
+    /*
+    public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;
+        }
+        return num;
+    }
+     */
+    // time = O(N)
+    // space = O(1)
+}
