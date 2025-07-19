c58894c7345522a48a36eca4e9b01ca731e748a6
AJINKYA <ajinkyacodes@gmail.com>
2025-07-19 09:54:01 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M07_July2025/L250719_Medium1233/_Remove_SubFolders_from_the_Filesystem.java
@@ -0,0 +1,33 @@
+package M07_July2025.L250719_Medium1233;
+/*
+Link: https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/description/
+1233. Remove Sub-Folders from the Filesystem
+ */
+import java.util.*;
+public class _Remove_SubFolders_from_the_Filesystem {
+    // Approach: Sorting
+    public List<String> removeSubfolders(String[] folder) {
+        // Sort the folders alphabetically
+        Arrays.sort(folder);
+
+        // Initialize the result list and add the first folder
+        List<String> result = new ArrayList<>();
+        result.add(folder[0]);
+
+        // Iterate through each folder and check if it's a sub-folder of the last added folder in the result
+        for (int i = 1; i < folder.length; i++) {
+            String lastFolder = result.get(result.size() - 1);
+            lastFolder += '/';
+
+            // Check if the current folder starts with the last added folder path
+            if (!folder[i].startsWith(lastFolder)) {
+                result.add(folder[i]);
+            }
+        }
+
+        // Return the result containing only non-sub-folders
+        return result;
+    }
+    // time = O(N⋅LlogN)
+    // space = O(N⋅L)
+}
