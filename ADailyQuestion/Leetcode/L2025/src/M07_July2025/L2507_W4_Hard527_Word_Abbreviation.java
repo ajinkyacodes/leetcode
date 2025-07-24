5fb6df2fd67329d510ed5d21558d0923e273d4fb
AJINKYA <ajinkyacodes@gmail.com>
2025-07-25 00:52:05 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M07_July2025/L2507_W4_Hard527_Word_Abbreviation.java
@@ -0,0 +1,77 @@
+package M07_July2025;
+/*
+Link: https://leetcode.com/problems/word-abbreviation/description/
+527. Word Abbreviation
+ */
+import java.util.*;
+public class L2507_W4_Hard527_Word_Abbreviation {
+    /*
+    public List<String> wordsAbbreviation(List<String> words) {
+        Map<String, List<IndexedWord>> groups = new HashMap();
+        String[] ans = new String[words.size()];
+
+        int index = 0;
+        for (String word: words) {
+            String ab = abbrev(word, 0);
+            if (!groups.containsKey(ab))
+                groups.put(ab, new ArrayList());
+            groups.get(ab).add(new IndexedWord(word, index));
+            index++;
+        }
+
+        for (List<IndexedWord> group: groups.values()) {
+            TrieNode trie = new TrieNode();
+            for (IndexedWord iw: group) {
+                TrieNode cur = trie;
+                for (char letter: iw.word.substring(1).toCharArray()) {
+                    if (cur.children[letter - 'a'] == null)
+                        cur.children[letter - 'a'] = new TrieNode();
+                    cur.count++;
+                    cur = cur.children[letter - 'a'];
+                }
+            }
+
+            for (IndexedWord iw: group) {
+                TrieNode cur = trie;
+                int i = 1;
+                for (char letter: iw.word.substring(1).toCharArray()) {
+                    if (cur.count == 1) break;
+                    cur = cur.children[letter - 'a'];
+                    i++;
+                }
+                ans[iw.index] = abbrev(iw.word, i-1);
+            }
+        }
+
+        return Arrays.asList(ans);
+    }
+
+    public String abbrev(String word, int i) {
+        int N = word.length();
+        if (N - i <= 3) return word;
+        return word.substring(0, i+1) + (N - i - 2) + word.charAt(N-1);
+    }
+    */
+}
+
+/*
+class TrieNode {
+    TrieNode[] children;
+    int count;
+    TrieNode() {
+        children = new TrieNode[26];
+        count = 0;
+    }
+}
+class IndexedWord {
+    String word;
+    int index;
+    IndexedWord(String w, int i) {
+        word = w;
+        index = i;
+    }
+}
+
+// time = O(C)
+// space = O(C)
+ */
