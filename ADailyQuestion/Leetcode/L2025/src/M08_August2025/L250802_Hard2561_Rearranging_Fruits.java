735ec2804a19610154edf8305b19b5b213bfdc8f
AJINKYA <ajinkyacodes@gmail.com>
2025-08-02 22:51:28 +0530

    code added

---
+++ ADailyQuestion/Leetcode/L2025/src/M08_August2025/L250802_Hard2561_Rearranging_Fruits.java
@@ -0,0 +1,38 @@
+package M08_August2025;
+/*
+Link: https://leetcode.com/problems/rearranging-fruits/description/
+2561. Rearranging Fruits
+ */
+import java.util.*;
+public class L250802_Hard2561_Rearranging_Fruits {
+    public long minCost(int[] basket1, int[] basket2) {
+        TreeMap<Integer, Integer> freq = new TreeMap<>();
+        int m = Integer.MAX_VALUE;
+        for (int b1 : basket1) {
+            freq.put(b1, freq.getOrDefault(b1, 0) + 1);
+            m = Math.min(m, b1);
+        }
+        for (int b2 : basket2) {
+            freq.put(b2, freq.getOrDefault(b2, 0) - 1);
+            m = Math.min(m, b2);
+        }
+
+        List<Integer> merge = new ArrayList<>();
+        for (var entry : freq.entrySet()) {
+            int count = entry.getValue();
+            if (count % 2 != 0) return -1;
+            for (int i = 0; i < Math.abs(count) / 2; i++) {
+                merge.add(entry.getKey());
+            }
+        }
+
+        Collections.sort(merge);
+        long res = 0;
+        for (int i = 0; i < merge.size() / 2; i++) {
+            res += Math.min(2 * m, merge.get(i));
+        }
+        return res;
+    }
+    // time = O(nlogn).
+    // space = O(n).
+}
